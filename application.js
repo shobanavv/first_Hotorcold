d3a6b928fefa1a672824a05c32313db52ce1a460
shobanavv <shobanavv@gmail.com>
2013-12-09 23:59:58 -0600

    changed layout

--- application.js
+++ application.js
@@ -1,84 +1,157 @@
-$(document).ready(function(){
-   var key=Math.floor((Math.random()*100)+1); //creating key when the page loads. 
+$(document).ready(function() {
+
+    var key = randomNumber(1,100);
     $("#player_guess").focus();
-    var guessNum;
+    var pre_diff=100;
+    var clicks=0;
+    var current_diff=0;
     var your_try = [];
-    your_try.lenght=0;
-     var msg;
-     var clicks=0;
-    var difference= function(a,b) {
-        return Math.abs(a-b)
-        }
+    var msg;
 
-   
-   var run_play = function(){
-    guessNum= Math.floor($("#player_guess").val());
-   
-    $("#player_guess").val("");//clear inputfield after button click.
-     $("#player_guess").focus();//cursor position ready in inoutfield.
-     if (your_try.length > 10) {
+//Finding difference between key with user guess.
+var difference= function(a,b) {
+        return Math.abs(a-b);
+ }       
+
+//creating key when the page loads.
+function randomNumber(from,to) {
+    return Math.floor((Math.random()*(to-from)+from));  
+}
+
+//Main game calculations. 
+var run_play = function(key) {
+    
+    guessNum= parseInt($("#player_guess").val());       //geting number from user.
+    $("#player_guess").val("");                         //clear input field after drop button click or enter key.
+    $("#player_guess").focus();                         //cursor position ready in inputfield.
+
+    //Displaying current 10 values in beeker. Just want to have current guesses.
+    if (your_try.length > 10) {
         your_try.shift();
-     }
-    
-    console.log("Answer is  "+key);
-    
-    clicks=clicks+1;
-    
-    
-    var diff = difference(key,guessNum);
-   
-        console.log("Difference   "+ diff);
-        
-        if (diff>=1 && diff<=5) {
-            msg = " Very close - Boiling";
-            $("#message").css("background-color"," #ff0000");
-        }
-        else if (diff>=6 && diff<=10) { 
-            msg = " Hot";
-            $("#message").css("background-color"," #cc3333");
-        }
-       else if (diff>10 && diff<=20) {
-        msg = "Getting Hotter";
-        $("#message").css("background-color"," #ff6633");
-       }
-       else if (diff>20 && diff<=30) {
-        msg = "warm";
-        $("#message").css("background-color"," #ffff66");
-       }
-        else if (diff>30 && diff<=40) {
-         msg= "Normal";
-         $("#message").css("background-color"," #ccffff");
-        }
-        else if (diff>40 && diff<=50) {
-            msg = "cold";
-            $("#message").css("background-color"," #a7d3ff");
-        }
-        else {
-            msg = "very cold";
-            $("#message").css("background-color"," #0066ff");
-        }
+    }
 
-     
-        if (key==guessNum) {
+    //checking for number between 1 and 100.
+    if (isNaN(guessNum) || guessNum ==" " || guessNum>100||guessNum==0) {
+        $("#comments").val("Enter a valid number between 1 and 100." );
+        $("#beeker").css("background-color","#05f2ee");
+        return;
+    } 
+
+    your_try.push(guessNum); //storing all guessed number in an array.
+    $("#your_guess").val(your_try); //Displaying guessed numbers into beeker.
+
+    
+    clicks=clicks+1; //Counting all tries. To show player, in how many tries they found the number I thought.
+
+    //User guessed it in first try?
+     if (key==guessNum) {
+            if(clicks==1) {      //if player find answer in first try. 
+                msg= "Woo Hoo!! You got it. You took only "+ clicks + " try. Cool!!!";
+                 $("#beeker").css("background-color"," #5EAD5E");
+
+            } else {
                 msg= "Woo Hoo!!!! You got it. You took  "+ clicks + "  tries.";
-                 $("#message").css("background-color"," #00ff00");
-             }
+                 $("#beeker").css("background-color"," #48F50A");
+                }
 
-        if (isNaN(guessNum) || guessNum ==" " || guessNum>100) {
-             $("#comments").val("Enter a valid number between 1 and 100." );
-              $("#message").css("background-color"," #ffffff");
-        }
-         else {
-            your_try.push(guessNum); //storing all guessed number in an array.
-          $("#comments").val(msg);
-            $("#your_guess").val(your_try); //Displaying all guessed numbers.
-        }
-    };
-        $("#play").click(run_play);
-        $("#player_guess").keydown(function(enter_key){
-            if (enter_key.keyCode === 13) {
-                run_play();
-            }
-        });
+    } else {      
+            current_diff = difference(key,guessNum);      //comparing current number with key.  
+            console.log("Difference   "+ current_diff);
+
+            if(current_diff<pre_diff || guessNum-key<0) {
+                if(current_diff<5) {
+                        
+                    msg="Boiling";
+                     $("#beeker").css("background-color"," #DE0404");
+                
+                } else if(current_diff<10) {
+                        
+                    msg="Hot";
+                    $("#beeker").css("background-color"," #F53D0A");
+                  
+                    } else if(current_diff<20) {
+                        
+                        msg=" Getting Hotter";
+                        $("#beeker").css("background-color"," #F5770A");
+                    
+                        } else if (current_diff<30) {
+                         
+                            msg="Warm.";
+                             $("#beeker").css("background-color"," #E8E180");
+                            
+                            } else {
+                            
+                                msg="Normal.";
+                                $("#beeker").css("background-color"," #F0E5A3");
+                                
+                                }
+                
+            } else if(current_diff<5) {
+                    
+                msg=" Answer is around the corner. You are moving away";
+                $("#beeker").css("background-color","#C4F0A3");
+                
+                    } else if(current_diff<10) {
+                                
+                        msg="Cold. ";
+                        $("#beeker").css("background-color"," #A3C0F0");
+                            
+                        } else if(current_diff<25) {
+                                msg=" Colder"; 
+                                $("#beeker").css("background-color"," #80ABE8"); 
+                               
+                            } else {
+
+                                 msg="Freezing.";
+                                 $("#beeker").css("background-color"," #80CAE8");
+                                }
+                
+             pre_diff=current_diff;
+
+    }//first try - else ends here.
+
+    $("#comments").val(msg);
+
+};//run_play function ends here.
+
+//Start Over button click calls following function.
+var play_again = function() {
+        key = randomNumber(1,100);
+        your_try = [];
+        pre_diff=100;
+        current_diff=0;
+        msg=" ";
+        clicks=0;
+        $("#player_guess").focus();
+        $("#your_guess").val(your_try);
+        $("#comments").val(msg);
+        $("#beeker").css("background-color","#05f2ee");
+};
+//Displays answer when give up button clicked.
+var giveup = function() {
+   
+    $("#comments").val("Answer is " + key +".");
     
+    };
+
+    // accepts user number when enter key pressed by calling run_play funtion.
+    $("#player_guess").keydown(function(enter_key) {
+            if (enter_key.keyCode === 13) {
+                run_play(key);
+            }
+    });
+   
+   //accepts number when the Drop button clicked.
+   $("#play").click(function() {
+        run_play(key);
+    });  
+
+    // calling function after Start over button clicked.
+   $("#start_over").click(function() {
+    
+        play_again();
+    }); //calling function give up to give answer.
+   $("#give_up").click(function(){
+    giveup();
+   });
 });
